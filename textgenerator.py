6068b252cc76bcbcb58d4048c349fa5d34c214e0
Alex-13-13 <132003501+Alex-13-13@users.noreply.github.com>
2023-07-22 18:28:36 +0200

    Update textgenerator.py

--- textgenerator.py
+++ textgenerator.py
@@ -3,6 +3,9 @@ import os
 import re
 from dotenv import load_dotenv, find_dotenv
 import json
+import ast
+import time
+
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.getenv('OPENAI_TOKEN')
@@ -16,37 +19,73 @@ def get_completion(prompt, temp,model="gpt-3.5-turbo"):
         max_tokens=2200
     )
     return response.choices[0].message["content"]
-def Creadortexto(title,duration):
-    minwords=float(duration)*150
-    maxwords=minwords+100
-    tru=True
-    prompt1 = f"""
-    Your task is to generate a text oriented to a general public about the subject of the title.
 
-    Generate the text using the title below, which is delimited by triple 
-    backticks, in at most {minwords} words.
+def Creadortexto(title,duration,top):
+    if duration>1:
+        minwords=int(float(duration)*160)
+    else:
+        minwords=100
+    maxwords=minwords+int(0.1*minwords)
+    menor=True
+    mayor=True
+    if top=="si" or "Si" or "sí" or "Sí":
+        prompt1 = f"""
+        Tu trabajo es crear un top original de {minwords} palabras, debes crear el top en base al titulo de abajo entre triple tildes.
+        Numera cada posición del top en orden descendente(3, 3a posicion. 2, 2a posicion. 1, 1a posicion).
+        Adapta todo lo necesario el top para que no exceda las {maxwords} palabras en total.
 
-    Title: ```{title}```
-    """
-    response = get_completion(prompt1,0.77)
+        Titulo: ```{title}```
+        """
+    else:
+        prompt1 = f"""
+        Tu trabajo es generar un texto de {minwords} palabras, orientado a un publico general sobre el tema del titulo.
+
+        Genera el texto usando el titulo de abajo, el cual se encuentra entre triple tildes, en como maximo {maxwords} palabras.
+
+        Titulo: ```{title}```
+        """
+    response = get_completion(prompt1,0)
     i=0
     palabras=len(re.findall(r'\w+', response))
-    while tru:
+    if palabras>=minwords:
+            menor=False
+    while menor:
         prompt2 = f"""
-        Your task is to rewrite a text and do it longer.
+        Tu trabajo es reescribir el texto dado y alargarlo.
 
-        Rewrite the text, delimited by triple backticks, so it's longer than the original but no longer than {maxwords} words.
+        Reescribe el texto, delimitado por triple tildes, para que sea más largo que el original pero no más de {maxwords} palabras.
 
-        Text: ```{response}```
+        Texto: ```{response}```
         """
-        response = get_completion(prompt2,0.77)
+        response = get_completion(prompt2,1)
         palabras=len(re.findall(r'\w+', response))
-        i=i+1
         if palabras>=minwords or i>4:
-            tru=False
+            menor=False
+        i=i+1
+    
+    if palabras<=maxwords:
+        mayor=False
+    while mayor:
+        prompt2 = f"""
+        Tu trabajo es resumir el texto dado en {minwords}-{maxwords} palabras.
+
+        Reescribe el texto, delimitado por triple tildes, para que sea más cortp que el original pero mayor de {minwords} palabras.
+
+        Texto: ```{response}```
+        """
+        response = get_completion(prompt2,1)
+        palabras=len(re.findall(r'\w+', response))
+        if palabras<=maxwords or i>4:
+            mayor=False
+        i=i+1
+
     return response
 
+    
+
 def jasonmomoa(guion):
+    division=[]
+    tematicas=[]
     prompt=f"""
         Your task is to write a short 50 character description in spanish of the text below, which is delimited by triple backticks.
 
@@ -62,13 +101,20 @@ def jasonmomoa(guion):
         Your task is to divide the text below, which is delimited by triple backticks, into short 20 word (no longer than 25 words) parts (it must be done so in a way in which if one unites all of the parts, it generates the original text) and put them in a Python list.
         Text: ```{guion}```
         """
-    division=get_completion(prompt3,0.2)
-    prompt4=f"""
-        Your task is to examine the sentiment/theme of each element of the list below (each sentiment/theme must be at most 3 words long), which is delimited by triple backticks, create a Python list containing these sentiments/themes in order translated to english. The number of elements in this list must be the same as the number as the number of elements in the list below delimited by triple backticks.
+    pdivision=get_completion(prompt3,0)
+    division=ast.literal_eval(pdivision)
+    
+    for i,text in enumerate(division):
+        prompt4=f"""
+            Your task is to examine the sentiment of the text below, which is delimited by triple backticks(for exemple: happy, erotic, conflict).
+            Examine the sentiment of the text and resume it in at most 2 or 3 words. 
+            Your answer must only be the return of the resultant sentiment(always in english).
 
-        Text: ```{division}```
-        """
-    tematicas=get_completion(prompt4,0.1)
+            Text: ```{text}```
+            """
+        tematicas.append(get_completion(prompt4,0))
+        i+=1
+        time.sleep(5)
     
     return [descripcion,tags,division,tematicas]
 
